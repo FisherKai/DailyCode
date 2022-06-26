f413ee2466a2fc406eb8caae2b1e16af4a72379b
yukai pc <1079869931@qq.com>
2022-06-26 23:57:46 +0800

    [update]-图片水印-202206262357

--- 图片加水印/index.js
+++ 图片加水印/index.js
@@ -8,6 +8,39 @@ function bindEvent() {
 
     uploadEle.addEventListener('change', function (e) {
         let file = e.target.files[0];
-        containerEle.appendChild(file)
+
+        // file 转 img
+        // const reader = new FileReader();
+        // reader.readAsDataURL(file);//读取图像文件 result 为 DataURL, DataURL 可直接 赋值给 img.src
+        // reader.onload = function (event) {
+        //     var img = document.createElement("img");
+        //     img.src = event.target.result;//base64
+        //     containerEle.appendChild(img);
+        // }
+
+        // file 转 canvas
+        var reader = new FileReader();
+        reader.readAsDataURL(file);//读取图像文件 result 为 DataURL, DataURL 可直接 赋值给 img.src
+        reader.onload = function (event) {
+            var image = new Image();
+            image.src = event.target.result;
+            image.onload = function () {
+                var canvas = document.createElement("canvas");
+                var imageCanvas = canvas.getContext("2d");
+                imageCanvas.drawImage(image, 0, 0);
+                const img = canvasToImage(canvas);
+                console.log(img);
+                containerEle.appendChild(img);
+            }
+        }
     })
+}
+
+/**
+ * @param {*} canvas 
+ */
+function canvasToImage(canvas) {
+    var image = document.createElement("img");
+    image.src = canvas.toDataURL("image/*");//base64
+    return image;
 }
