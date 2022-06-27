28193c6a7cec74f82954be57f0605d61de63436b
余凯 <yukai@egova.com.cn>
2022-06-27 14:54:06 +0800

    [update]-图片水印-202206271454

--- 图片加水印/index.js
+++ 图片加水印/index.js
@@ -8,6 +8,7 @@ function bindEvent() {
 
     uploadEle.addEventListener('change', function (e) {
         let file = e.target.files[0];
+        console.log(file);
 
         // file 转 img
         // const reader = new FileReader();
@@ -22,15 +23,20 @@ function bindEvent() {
         var reader = new FileReader();
         reader.readAsDataURL(file);//读取图像文件 result 为 DataURL, DataURL 可直接 赋值给 img.src
         reader.onload = function (event) {
-            var image = new Image();
+            var image = document.createElement("img");;
             image.src = event.target.result;
+            // containerEle.appendChild(image);
             image.onload = function () {
                 var canvas = document.createElement("canvas");
+                canvas.width = image.width;
+                canvas.height = image.height;
                 var imageCanvas = canvas.getContext("2d");
-                imageCanvas.drawImage(image, 0, 0);
-                const img = canvasToImage(canvas);
-                console.log(img);
-                containerEle.appendChild(img);
+                imageCanvas.drawImage(image, 0, 0,image.width,image.height);
+                imageCanvas.fillStyle = "red";
+                imageCanvas.textBaseline = "middle";
+                imageCanvas.fillText("仅供烟台入户使用", image.width/2, image.height/2);
+                // const img = canvasToImage(canvas);
+                containerEle.appendChild(canvas);
             }
         }
     })
