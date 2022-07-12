ce3a4a69863720462cdf4f6c14619931852c5a8a
余凯 <yukai@egova.com.cn>
2022-07-12 13:34:49 +0800

    [add]---拖拽组件

--- VueDemo/demo1/src/App.vue
+++ VueDemo/demo1/src/App.vue
@@ -1,22 +1,29 @@
 <template>
   <section>
-    <CaseOne />
-    <MyList msg="Welcome to Your Vue.js App" />
+    <!-- <CaseOne />
+    <MyList msg="Welcome to Your Vue.js App" /> -->
+    <DragComponentOne />
   </section>
 </template>
 
 <script>
-import CaseOne from "./components/CaseOne.vue";
-import MyList from "./components/MyList.vue";
+// import CaseOne from "./components/CaseOne.vue";
+import DragComponentOne from './components/DragComponentOne.vue';
+// import MyList from "./components/MyList.vue";
 
 export default {
   name: "App",
   components: {
-    MyList,
-    CaseOne,
+    // MyList,
+    // CaseOne,
+    DragComponentOne
   },
 };
 </script>
 
 <style>
+html,
+body{
+  padding: 0
+}
 </style>
---
+++ VueDemo/demo1/src/components/DragComponentOne.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="drag-of-container">
+    <div id="slot" @drag="slotDrag" draggable="true" ref="slot"></div>
+    <div id="block" @drag="blockDrag" draggable="true" ref="block"></div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "DragComponentOne",
+  setup() {
+   
+  },
+  methods: {
+    slotDrag(e) {
+      console.log(e);
+    },
+    blockDrag(e) {
+      console.log(e);
+    },
+  },
+};
+</script>
+
+<style>
+.drag-of-container {
+  width: 100%;
+  height: 100%;
+}
+#slot {
+  position: fixed;
+  border: 2px solid black;
+  width: 100px;
+  height: 100px;
+}
+#block {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  background: orange;
+  width: 100px;
+  height: 100px;
+}
+</style>
