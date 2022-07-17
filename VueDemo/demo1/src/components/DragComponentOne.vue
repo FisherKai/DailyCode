3406c2893c34516b7181d6b84467ff71a910fd0f
fisherkai <18375183097@163.com>
2022-07-17 17:25:46 +0800

    [update]---拖拽功能实现

--- VueDemo/demo1/src/components/DragComponentOne.vue
+++ VueDemo/demo1/src/components/DragComponentOne.vue
@@ -1,7 +1,9 @@
 <template>
-  <section class="drag-of-container">
-    <div id="slot" @dragenter="slotDragEnter" @dragleave="slotDragLeave" draggable="true" ref="slot"></div>
-    <div id="block" @drag="blockDrag" draggable="true" ref="block"></div>
+  <section class="drag-of-container" id="drag-of-container">
+    <div id="slot" @dragenter="slotDragEnter" @dragover="slotDragOver" @drop="slotDrop" @dragleave="slotDragLeave"
+      draggable="true" ref="slot">
+    </div>
+    <div id="block" @drag="blockDrag" @dragover="blockDragOver" @drop="blockDrop" draggable="true" ref="block"></div>
   </section>
 </template>
 
@@ -15,13 +17,35 @@ export default {
   },
   methods: {
     slotDragEnter(e) {
-      console.log(e, this.$refs.slot.appendChild,this.$refs.block);
+      console.log(e);
       this.$refs.slot.style.border = `2px solid red`;
-      this.$refs.slot.appendChild(this.$refs.block);
+    },
+    slotDragOver(e) {
+      // 不在dragOver事件阻止默认行为 drop事件无法生效
+      e.preventDefault();
+    },
+    blockDragOver(e) {
+      e.preventDefault();
+    },
+    blockDrop() {
+      const container = document.getElementById("drag-of-container");
+      const slot = document.getElementById("slot");
+      const block = document.getElementById("block");
+      block.style.position = "fixed";
+      slot.removeChild(block);
+      container.append(block)
+    },
+    slotDrop(e) {
+      console.log(e, this.$refs.slot.hasChildNodes());
+      const slot = document.getElementById("slot");
+      const block = document.getElementById("block");
+      block.style.position = "static";
+      slot.append(block)
     },
     slotDragLeave(e) {
       console.log(e);
       this.$refs.slot.style.border = `2px solid black`;
+
     },
     blockDrag(e) {
       e
